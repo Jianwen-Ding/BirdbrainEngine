35d4541f047a98ccd3e5f4f87eb3a3f6a7f38be1
Jianwen Ding <jianwen_ding@icloud.com>
2024-12-20 18:07:09 -0500

    print

--- include/entitylogic/Entity.hpp
+++ include/entitylogic/Entity.hpp
@@ -17,27 +17,29 @@ class Entity{
         // Gets a pointer to a component of type V within the entity.
         // Gets the most recently added component of type V.
         // Will return a nullptr if no such component exist.
-        template<typename V, typename = std::enable_if_t<std::is_base_of<T, V>::value>>>
+        template<typename V>
         V* getComponent();
         
         // Gets a pointer to every type component of type V within the entity.
         // Will return an empty vector if no such componenta exist.
-        template<typename V, typename = std::enable_if_t<std::is_base_of<T, V>::value>>>
+        template<typename V>
         std::vector<V*> getComponents();
 
+        // Removes the given component from the Entity and deletes it.
+        // Returns whether the component actually ever existed within the entity.
+        template<typename V>
+        bool removeComponent(V* givenComponent);
+
+    protected:
+        // Calls init on all of the components within the entity
+        void init();
+        // Calls start on all of the components within the entity
+        void start();
     private:
+
         // List of components of a given type within the value.
         // The farther out in the vector the component 
         std::vector<T> componentVec;
-
-
-        // Removes the given component from the Entity if needed, only usable by the component itself
-        template<typename V, typename = std::enable_if_t<std::is_base_of<T, V>::value>>>
-        bool removeComponent(V* givenComponent);
-
-        // Only the 
-        friend class Component;
-
 };
 
 #endif 
